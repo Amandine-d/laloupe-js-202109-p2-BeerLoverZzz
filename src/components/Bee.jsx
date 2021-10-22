7d5e98602abfb7959710c33f8746c4cd7ce8d969
Amandine <amandine.druart@gmail.com>
2021-10-22 10:00:07 +0200

    Add images + components Footer/Header/Bee/Home

---
+++ src/components/Bee.jsx
@@ -0,0 +1,11 @@
+import img from '../images/little-bee.png';
+
+function Bee() {
+  return (
+    <>
+      <img src={img} alt="little-bee" className="maya" />
+    </>
+  );
+}
+
+export default Bee;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import img from '../images/title-beer-loverzzz.png';
+
+function Header() {
+  return (
+    <>
+      <img src={img} alt="title" className="title" />
+    </>
+  );
+}
+export default Header;
