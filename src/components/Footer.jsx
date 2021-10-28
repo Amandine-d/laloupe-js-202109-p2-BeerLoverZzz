3c9bfd90efefeebf0ff76bc6f94f1e83fcdf383b
Sidney <sidney.croset@hotmail.com>
2021-10-28 17:40:11 +0200

    Ajout du background et  des 3 liens

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,32 @@
 function Footer() {
   return (
     <div className="Footer">
-      <p className="Footer">Footer</p>
+      <h3>Forums:</h3>
+      <div className="link">
+        <ul>
+          <li>
+            <a href="https://untappd.com/" target="blank">
+              Untappd
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.youtube.com/channel/UCwzxu1j19ICnfW5zrBvUw-w"
+              target="blank"
+            >
+              Une bière et Jivay
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.happybeertime.com/blog/2017/11/02/guide-complet-faire-1ere-biere-a-maison/"
+              target="blank"
+            >
+              Comment Brasser sa bière
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
