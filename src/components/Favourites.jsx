8d5ca6eff13072e7b131249a01e0ef90071f7cde
Amandine <amandine.druart@gmail.com>
2021-11-05 11:56:31 +0100

    Corrected problems after merge

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import ChoseBeer from './ChoseBeer';
-import Bee from './Bee';
 
 function Favourites() {
   return (
     <div>
-      <p>This is Favourites</p>
-      <ChoseBeer />
-      <Bee />
+      <p style={{ height: '80vh' }}>This is Favourites</p>
     </div>
   );
 }
