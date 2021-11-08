fb942b03ccb27d809fb822f588e45776e0042eea
Amandine <amandine.druart@gmail.com>
2021-11-08 21:13:23 +0100

    Added a like button in images, like buttons on cards are clickable, key for id on it

--- src/components/beers/DisplayBeer.jsx
+++ src/components/beers/DisplayBeer.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import like from '../../images/like.png';
+import dislike from '../../images/dislike.png';
 import './DisplayBeer.css';
 
 const DisplayBeer = ({ beer }) => {
   const [favourite, setFavourite] = useState(false);
   const handleClickFavourite = () => setFavourite(!favourite);
-  const handleKeyUp = () => setFavourite(!favourite);
   return (
     beer && (
       <div className="card__beer">
@@ -22,16 +22,32 @@ const DisplayBeer = ({ beer }) => {
           <p className="beer__right__ibu">{beer.ibu}</p>
           <p className="beer__right__tagline">{beer.tagline}</p>
           <p className="beer__right__description">{beer.desription}</p>
-          <button
-            id="button__like"
-            onClick={handleClickFavourite}
-            onKeyUp={handleKeyUp}
-            type="button"
-            aria-label="save"
-            className={favourite ? 'isFavourite' : 'notFavourite'}
-          >
-            <img src={like} alt="like button" className="button__like" />
-          </button>
+          <div>
+            {favourite && (
+              <button
+                id="favourite"
+                className="button__favourite"
+                onClick={handleClickFavourite}
+                aria-label="fav"
+                type="button"
+                key={beer.id}
+              >
+                <img src={like} alt="like" className="button__like" />
+              </button>
+            )}
+            {!favourite && (
+              <button
+                id="notFav"
+                className="button__favourite"
+                onClick={handleClickFavourite}
+                aria-label="fav"
+                type="button"
+                key={beer.id}
+              >
+                <img src={dislike} alt="dislike" className="button__like" />
+              </button>
+            )}
+          </div>
         </div>
       </div>
     )
